e35af8dfcc6729dfa09eaa3f88220bb36501b46e
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2025-10-22 15:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin:
       process.env.FRONTEND_URL ||
-      "https://frontendcraft-brsq.vercel.app/authPage",
+      "https://craft-clone-frontend-qoix.vercel.app/authpage",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
