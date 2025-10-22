c7f43f2dac5e7d69b3d3b37d9cc9c9dda2443f59
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2025-10-22 13:27:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 // ✅ CORS Config
 const allowedOrigins = [
   process.env.FRONTEND_URL, 
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "https://sweet-cupcake-1857ea.netlify.app/authpage"
 ];
 
 app.use(cors({
