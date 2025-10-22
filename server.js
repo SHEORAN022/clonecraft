06506bc550f3a9948a4f7a9f0e5991ab597d9810
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2025-10-22 13:19:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@
 // app.use(helmet());
 // app.use(
 //   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     origin: process.env.FRONTEND_URL || "https://sweet-cupcake-1857ea.netlify.app/authpage",
 //     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 //     allowedHeaders: ["Content-Type", "Authorization"],
 //     credentials: true,
